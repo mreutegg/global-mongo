47a586cbeb471d0a0f15b11827d30d796f6d5e67
Tomek Rekawek <rekawek@adobe.com>
2016-01-19 14:16:56 +0100

    Remove the volumes in the kill.sh script.

--- kill.sh
+++ kill.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+docker ps --filter 'label=global.clustering.type=mongo' -q | while read id
+do
+  echo "Removing volume on $id"
+  docker cp scripts/clean.sh $id:/tmp
+  docker exec $id /tmp/clean.sh
+done
+
 docker-compose kill
 docker-compose rm -f
