e639c9141fe79671e3aa2ff135b17e6eeb2ac476
Tomek Rękawek <rekawek@adobe.com>
2016-06-16 13:13:14 +0200

    Support running locally.

--- init.sh
+++ init.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+MACHINE_IP=$(docker-machine ip 2>/dev/null || echo "127.0.0.1")
+
 docker-compose up -d
 
 echo "Initiating replica set"
-mongo $(docker-machine ip):27017 ./scripts/replica_set.js
+mongo $MACHINE_IP:27017 ./scripts/replica_set.js
 
 docker-compose logs
--- set_latency.sh
+++ set_latency.sh
@@ -2,15 +2,15 @@
 
 LATENCY=$1
 JITTER=${2:-0}
-IP=$(docker-machine ip)
+MACHINE_IP=$(docker-machine ip 2>/dev/null || echo "127.0.0.1")
 
 for proxy_name in $(curl -s $IP:8474/proxies | jq -r 'keys[]')
 do
   if [ -n "$LATENCY" ]; then
     echo "Set latency ${LATENCY} with jitter ${JITTER} on ${proxy_name}"
-    curl -s -i -d '{"enabled":true, "latency":'${LATENCY}', "jitter": '${JITTER}'}' "${IP}:8474/proxies/${proxy_name}/downstream/toxics/latency" > /dev/null
+    curl -s -i -d '{"enabled":true, "latency":'${LATENCY}', "jitter": '${JITTER}'}' "${MACHINE_IP}:8474/proxies/${proxy_name}/downstream/toxics/latency" > /dev/null
   else
     echo "Disable latency on ${proxy_name}"
-    curl -s -i -d '{"enabled":false}' "${IP}:8474/proxies/${proxy_name}/downstream/toxics/latency" > /dev/null
+    curl -s -i -d '{"enabled":false}' "${MACHINE_IP}:8474/proxies/${proxy_name}/downstream/toxics/latency" > /dev/null
   fi
 done
