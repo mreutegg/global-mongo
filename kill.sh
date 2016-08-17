4095eb98e96d5215e1bd56ef06f5112b7fd842ea
Tomek Rękawek <rekawek@adobe.com>
2016-08-17 13:33:48 +0200

    Use named volumes

--- kill.sh
+++ kill.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-docker ps --filter 'label=global.clustering.type=mongo' -q | while read id
-do
-  echo "Removing volume on $id"
-  docker cp scripts/clean.sh $id:/tmp
-  docker exec $id /tmp/clean.sh
-done
-
 docker-compose kill
 docker-compose rm -f
+docker volume rm $(docker volume ls | grep -E 'm[123]-((conf)|(data))') > /dev/null 2>&1
